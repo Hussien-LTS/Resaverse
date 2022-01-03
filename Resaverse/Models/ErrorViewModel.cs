7e4905eedb5d772d1a2f096c7de5d13f5e0bb8ce
fadiHB <eng.fadi.hb@gmail.com>
2022-01-03 19:59:14 +0200

    init

---
+++ Resaverse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Resaverse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
